cbe5fe308cf0daf4c10504e7e59d37f17927d7db
juaser <zxl@reachjunction.com>
2016-07-31 22:48:14 +0800

    first

---
+++ CalendarExample/app/src/main/java/com/calendarexample/MainActivity.java
@@ -0,0 +1,25 @@
+package com.calendarexample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.plugin.calendarweight.KCalendarPop;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final Button btn = (Button) findViewById(R.id.btn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new KCalendarPop(MainActivity.this, btn);
+            }
+        });
+    }
+}
